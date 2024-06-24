3cd8e0c119d02bc1a0e95d2439b093a3a6dcea37
Vladyslav <Vladyslav@VLADYSLAV>
2024-06-25 02:17:28 +0300

    Add project files.

---
+++ Libary/Data/DblibaryContext.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Libary.Data;
+
+public partial class DblibaryContext : DbContext
+{
+    public DblibaryContext()
+    {
+    }
+
+    public DblibaryContext(DbContextOptions<DblibaryContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
+
+    public virtual DbSet<AspNetRoleClaim> AspNetRoleClaims { get; set; }
+
+    public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
+
+    public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
+
+    public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
+
+    public virtual DbSet<AspNetUserToken> AspNetUserTokens { get; set; }
+
+    public virtual DbSet<Autor> Autors { get; set; }
+
+    public virtual DbSet<Delivery> Deliveries { get; set; }
+
+    public virtual DbSet<Epoch> Epoches { get; set; }
+
+    public virtual DbSet<Genre> Genres { get; set; }
+
+    public virtual DbSet<LibaryCheck> LibaryChecks { get; set; }
+
+    public virtual DbSet<Publication> Publications { get; set; }
+
+    public virtual DbSet<PublicationAutor> PublicationAutors { get; set; }
+
+    public virtual DbSet<Reader> Readers { get; set; }
+
+    public virtual DbSet<Region> Regions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=Vladyslav\\SQLEXPRESS; Database=DBLibary; Trusted_Connection=True; TrustServerCertificate=True; ");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AspNetRole>(entity =>
+        {
+            entity.HasIndex(e => e.NormalizedName, "RoleNameIndex")
+                .IsUnique()
+                .HasFilter("([NormalizedName] IS NOT NULL)");
+
+            entity.Property(e => e.Name).HasMaxLength(256);
+            entity.Property(e => e.NormalizedName).HasMaxLength(256);
+        });
+
+        modelBuilder.Entity<AspNetRoleClaim>(entity =>
+        {
+            entity.HasIndex(e => e.RoleId, "IX_AspNetRoleClaims_RoleId");
+
+            entity.HasOne(d => d.Role).WithMany(p => p.AspNetRoleClaims).HasForeignKey(d => d.RoleId);
+        });
+
+        modelBuilder.Entity<AspNetUser>(entity =>
+        {
+            entity.HasIndex(e => e.NormalizedEmail, "EmailIndex");
+
+            entity.HasIndex(e => e.NormalizedUserName, "UserNameIndex")
+                .IsUnique()
+                .HasFilter("([NormalizedUserName] IS NOT NULL)");
+
+            entity.Property(e => e.Email).HasMaxLength(256);
+            entity.Property(e => e.NormalizedEmail).HasMaxLength(256);
+            entity.Property(e => e.NormalizedUserName).HasMaxLength(256);
+            entity.Property(e => e.UserName).HasMaxLength(256);
+
+            entity.HasMany(d => d.Roles).WithMany(p => p.Users)
+                .UsingEntity<Dictionary<string, object>>(
+                    "AspNetUserRole",
+                    r => r.HasOne<AspNetRole>().WithMany().HasForeignKey("RoleId"),
+                    l => l.HasOne<AspNetUser>().WithMany().HasForeignKey("UserId"),
+                    j =>
+                    {
+                        j.HasKey("UserId", "RoleId");
+                        j.ToTable("AspNetUserRoles");
+                        j.HasIndex(new[] { "RoleId" }, "IX_AspNetUserRoles_RoleId");
+                    });
+        });
+
+        modelBuilder.Entity<AspNetUserClaim>(entity =>
+        {
+            entity.HasIndex(e => e.UserId, "IX_AspNetUserClaims_UserId");
+
+            entity.HasOne(d => d.User).WithMany(p => p.AspNetUserClaims).HasForeignKey(d => d.UserId);
+        });
+
+        modelBuilder.Entity<AspNetUserLogin>(entity =>
+        {
+            entity.HasKey(e => new { e.LoginProvider, e.ProviderKey });
+
+            entity.HasIndex(e => e.UserId, "IX_AspNetUserLogins_UserId");
+
+            entity.HasOne(d => d.User).WithMany(p => p.AspNetUserLogins).HasForeignKey(d => d.UserId);
+        });
+
+        modelBuilder.Entity<AspNetUserToken>(entity =>
+        {
+            entity.HasKey(e => new { e.UserId, e.LoginProvider, e.Name });
+
+            entity.HasOne(d => d.User).WithMany(p => p.AspNetUserTokens).HasForeignKey(d => d.UserId);
+        });
+
+        modelBuilder.Entity<Autor>(entity =>
+        {
+            entity.Property(e => e.AutorName).HasMaxLength(50);
+            entity.Property(e => e.Pseudonym).HasMaxLength(50);
+
+            entity.HasOne(d => d.Region).WithMany(p => p.Autors)
+                .HasForeignKey(d => d.RegionId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Autors_Regions");
+        });
+
+        modelBuilder.Entity<Delivery>(entity =>
+        {
+            entity.Property(e => e.PostOfficeAdress).HasMaxLength(50);
+
+            entity.HasOne(d => d.Reader).WithMany(p => p.Deliveries)
+                .HasForeignKey(d => d.ReaderId)
+                .HasConstraintName("FK_Deliveries_Readers");
+        });
+
+        modelBuilder.Entity<Epoch>(entity =>
+        {
+            entity.Property(e => e.EpochName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Genre>(entity =>
+        {
+            entity.Property(e => e.GenreName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<LibaryCheck>(entity =>
+        {
+            entity.Property(e => e.DateTime).HasColumnType("datetime");
+
+            entity.HasOne(d => d.Delivery).WithMany(p => p.LibaryChecks)
+                .HasForeignKey(d => d.DeliveryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_LibaryChecks_Deliveries");
+
+            entity.HasOne(d => d.Publication).WithMany(p => p.LibaryChecks)
+                .HasForeignKey(d => d.PublicationId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_LibaryChecks_Publications");
+        });
+
+        modelBuilder.Entity<Publication>(entity =>
+        {
+            entity.Property(e => e.Annotation).HasMaxLength(50);
+            entity.Property(e => e.BookName).HasMaxLength(50);
+
+            entity.HasOne(d => d.Epoch).WithMany(p => p.Publications)
+                .HasForeignKey(d => d.EpochId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Publications_Epoches");
+
+            entity.HasOne(d => d.Genre).WithMany(p => p.Publications)
+                .HasForeignKey(d => d.GenreId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Publications_Genres");
+        });
+
+        modelBuilder.Entity<PublicationAutor>(entity =>
+        {
+            entity.HasOne(d => d.Autor).WithMany(p => p.PublicationAutors)
+                .HasForeignKey(d => d.AutorId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_PublicationAutors_Autors");
+
+            entity.HasOne(d => d.Publication).WithMany(p => p.PublicationAutors)
+                .HasForeignKey(d => d.PublicationId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_PublicationAutors_Publications");
+        });
+
+        modelBuilder.Entity<Reader>(entity =>
+        {
+            entity.Property(e => e.Address).HasMaxLength(50);
+            entity.Property(e => e.Email).HasMaxLength(50);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+            entity.Property(e => e.NickName).HasMaxLength(50);
+            entity.Property(e => e.PhoneNumber).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Region>(entity =>
+        {
+            entity.Property(e => e.RegionName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Libary/Migrations/Dblibary/20240523164456_Identity.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Libary.Migrations.Dblibary
+{
+    /// <inheritdoc />
+    public partial class Identity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Epoches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EpochName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Epoches", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GenreName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Readers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NickName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Readers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Regions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RegionName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Regions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Publications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GenreId = table.Column<int>(type: "int", nullable: false),
+                    EpochId = table.Column<int>(type: "int", nullable: false),
+                    BookName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Annotation = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    PageCout = table.Column<int>(type: "int", nullable: true),
+                    Price = table.Column<int>(type: "int", nullable: true),
+                    Year = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Publications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Publications_Epoches",
+                        column: x => x.EpochId,
+                        principalTable: "Epoches",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Publications_Genres",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Deliveries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PostOfficeNumber = table.Column<int>(type: "int", nullable: true),
+                    PostOfficeAdress = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    ReaderId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deliveries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Deliveries_Readers",
+                        column: x => x.ReaderId,
+                        principalTable: "Readers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Autors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RegionId = table.Column<int>(type: "int", nullable: false),
+                    AutorName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Pseudonym = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Autors_Regions",
+                        column: x => x.RegionId,
+                        principalTable: "Regions",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LibaryChecks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PublicationId = table.Column<int>(type: "int", nullable: false),
+                    DeliveryId = table.Column<int>(type: "int", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LibaryChecks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LibaryChecks_Deliveries",
+                        column: x => x.DeliveryId,
+                        principalTable: "Deliveries",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_LibaryChecks_Publications",
+                        column: x => x.PublicationId,
+                        principalTable: "Publications",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PublicationAutors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    PublicationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PublicationAutors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PublicationAutors_Autors",
+                        column: x => x.Id,
+                        principalTable: "Autors",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_PublicationAutors_Publications",
+                        column: x => x.PublicationId,
+                        principalTable: "Publications",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Autors_RegionId",
+                table: "Autors",
+                column: "RegionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deliveries_ReaderId",
+                table: "Deliveries",
+                column: "ReaderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LibaryChecks_DeliveryId",
+                table: "LibaryChecks",
+                column: "DeliveryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LibaryChecks_PublicationId",
+                table: "LibaryChecks",
+                column: "PublicationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PublicationAutors_PublicationId",
+                table: "PublicationAutors",
+                column: "PublicationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Publications_EpochId",
+                table: "Publications",
+                column: "EpochId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Publications_GenreId",
+                table: "Publications",
+                column: "GenreId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LibaryChecks");
+
+            migrationBuilder.DropTable(
+                name: "PublicationAutors");
+
+            migrationBuilder.DropTable(
+                name: "Deliveries");
+
+            migrationBuilder.DropTable(
+                name: "Autors");
+
+            migrationBuilder.DropTable(
+                name: "Publications");
+
+            migrationBuilder.DropTable(
+                name: "Readers");
+
+            migrationBuilder.DropTable(
+                name: "Regions");
+
+            migrationBuilder.DropTable(
+                name: "Epoches");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+        }
+    }
+}
